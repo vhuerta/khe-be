7a06db2505ac1d8b5e92ca38ca22bc728989a20d
Victor Huerta <vhuertahnz@gmail.com>
2017-03-01 23:20:40 -0600

    Test

---
+++ static/js/bebel.0d2a8058007f994c2fa5.js
@@ -0,0 +1,2 @@
+webpackJsonp([2,3,4],[],[579]);
+//# sourceMappingURL=bebel.0d2a8058007f994c2fa5.js.map
---
+++ static/js/fetch.6d03a0099d703469d4b9.js
@@ -0,0 +1,2 @@
+webpackJsonp([3,2,4],[],[580]);
+//# sourceMappingURL=fetch.6d03a0099d703469d4b9.js.map
